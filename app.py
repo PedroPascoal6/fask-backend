d6b9793ea8aa82b51504a75d984df68beede4bd1
Pedro Pascoal <pedro.pascoal.4@gmail.com>
2018-11-12 02:45:30 +0000

    table politician updated and dictionary has been created
    the politician table have now two new fieds with respective politician names

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
+import sqlite3
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
+import json
+import sqlite3
 from json import dumps
 # from flask.ext.jsonpify import jsonify
 
@@ -15,7 +18,7 @@ api = Api(app)
 
 class Employees(Resource):
     def get(self):
-        conn = db_connect.connect()  # connect to database
+        #conn = db_connect.connect()  # connect to database
         query = [{"attr": {"id": "t1",
                            "name": "Wibeu",
                            "superiorid": "Wibeu",
@@ -64,10 +67,26 @@ class Employees(Resource):
                        }
                   ]
                   }, ]
-        querybd = conn.execute("select * from politician")  # This line performs query and returns json result
+
+        #querybd = conn.execute("select * from politician")  # This line performs query and returns json result
         #print (jsonify(query))
-        return {'employees': [i[1] for i in querybd.cursor.fetchall()]}  # Fetches first column that is Employee ID
-        #return jsonify(query)
+        #return {'employees': [i[1] for i in querybd.cursor.fetchall()]}  # Fetches first column that is Employee ID
+
+        con = sqlite3.connect("politiciansBD.db")
+        # Print the table contents
+        data={}
+        for row in con.execute("select * from politician"):
+            data[row[0]] = {"name": row[1],
+                            "superiorid": row[2],
+                            "superiorName": row[3],
+                            "subordinateid": row[4],
+                            "subordinateName": row[5]}
+        print (data)
+
+
+
+        return jsonify(data)
+
 
 
 api.add_resource(Employees, '/employees')  # Route_1
@@ -80,25 +99,34 @@ def after_request(response):
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
 
+def createJson():
+    data = {}
+    data['attr'] = {"id": "t1",
+                    "name": "Wibeu",
+                    "superiorid": "Wibeu",
+                    "subordinateid": "Curusego",
+                    }
+    json_data = json.dumps(data)
+
 
 def updateData():
     conn = db_connect.connect()  # connect to database
 
-    sql = ''' INSERT INTO politician VALUES(0,'Wibeu',null,1) '''
+    sql = ''' INSERT INTO politician VALUES(0,'Wibeu',null,'',1,'Curusego',1) '''
     conn.execute(sql)
-    sql = ''' INSERT INTO politician VALUES(1,'Curusego',1,2) '''
+    sql = ''' INSERT INTO politician VALUES(1,'Curusego',1,'Curusego',2,'Nalvar',1) '''
     conn.execute(sql)
-    sql = ''' INSERT INTO politician VALUES(2,'Nalvar',1,null) '''
+    sql = ''' INSERT INTO politician VALUES(2,'Nalvar',1,'Curusego',null,'',1) '''
     conn.execute(sql)
-    sql = ''' INSERT INTO politician VALUES(3,'Alberto',1,null) '''
+    sql = ''' INSERT INTO politician VALUES(3,'Alberto',1,'Curusego',null,'',1) '''
     conn.execute(sql)
-    sql = ''' INSERT INTO politician VALUES(4,'Bufiobere',1,null) '''
+    sql = ''' INSERT INTO politician VALUES(4,'Bufiobere',1,'Curusego',null,'',1) '''
     conn.execute(sql)
-    sql = ''' INSERT INTO politician VALUES(5,'Flunodae',0,6) '''
+    sql = ''' INSERT INTO politician VALUES(5,'Flunodae',0,'',6,'Osceo',1) '''
     conn.execute(sql)
-    sql = ''' INSERT INTO politician VALUES(6,'Osceo',5,null) '''
+    sql = ''' INSERT INTO politician VALUES(6,'Osceo',5,'Flunodae',null,'',1) '''
     conn.execute(sql)
-    sql = ''' INSERT INTO politician VALUES(7,'Envailslag',null,null) '''
+    sql = ''' INSERT INTO politician VALUES(7,'Envailslag',null,'',null,'',1) '''
     conn.execute(sql)
     return True
 
